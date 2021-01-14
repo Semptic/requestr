7821ffaa25caaa27058ea4a9fc7e17343e79a8a2
Stefan Kuhn <stefan@kuhn.bayern>
2021-01-14 20:27:53 +0100

    Add basic logging setup

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,5 +1,26 @@
+#[macro_use]
+extern crate lazy_static;
+extern crate clap_verbosity_flag;
+use log::{debug, error, info, trace, warn};
 use requestr_core::hello_world;
-
+use simplelog::{Config, TermLogger, TerminalMode};
+use structopt::StructOpt;
+lazy_static! {
+    static ref OPT: Opt = Opt::from_args();
+}
 fn main() {
+    TermLogger::init(OPT.verbose.log_level().unwrap().to_level_filter(), Config::default(), TerminalMode::Mixed).unwrap();
+
+    error!("Oh nooo!");
+    warn!("warn");
+    info!("info");
+    debug!("debug");
+    trace!("Trace");
+
     hello_world();
+}
+#[derive(Debug, StructOpt)]
+struct Opt {
+    #[structopt(flatten)]
+    verbose: clap_verbosity_flag::Verbosity,
 }
