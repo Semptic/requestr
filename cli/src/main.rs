3be3bf5444bc7bc1d02d5264ad318936031bd512
Stefan Kuhn <stefan@kuhn.bayern>
2021-01-14 20:36:41 +0100

    Use local time for logging

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -3,13 +3,17 @@ extern crate lazy_static;
 extern crate clap_verbosity_flag;
 use log::{debug, error, info, trace, warn};
 use requestr_core::hello_world;
-use simplelog::{Config, TermLogger, TerminalMode};
+use simplelog::{Config, ConfigBuilder, TermLogger, TerminalMode};
 use structopt::StructOpt;
 lazy_static! {
     static ref OPT: Opt = Opt::from_args();
 }
 fn main() {
-    TermLogger::init(OPT.verbose.log_level().unwrap().to_level_filter(), Config::default(), TerminalMode::Mixed).unwrap();
+    let config = ConfigBuilder::new()
+      .set_time_to_local(true)
+      .build();
+
+    TermLogger::init(OPT.verbose.log_level().unwrap().to_level_filter(), config, TerminalMode::Mixed).unwrap();
 
     error!("Oh nooo!");
     warn!("warn");
