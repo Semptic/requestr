04fe0e3ea0310b7ecf4d5f2c4cc3550c28baf480
Stefan Kuhn <stefan@kuhn.bayern>
2021-01-15 13:54:14 +0100

    Add templating engine

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -2,22 +2,35 @@
 extern crate lazy_static;
 extern crate serde_json;
 extern crate clap_verbosity_flag;
+use std::collections::HashMap;
+
 use log::{debug};
-use requestr_core::{load_request_definition, make_request};
+use requestr_core::{load_request_definition, load_request_template, make_request, validate_parameter};
 use serde_json::Value;
 use simplelog::{ConfigBuilder, TermLogger, TerminalMode};
 use structopt::StructOpt;
-lazy_static! {
-    static ref OPT: Opt = Opt::from_args();
-}
+
 fn main() {
+    lazy_static! {
+        static ref OPT: Opt = Opt::from_args();
+    }
+
     let config = ConfigBuilder::new()
       .set_time_to_local(true)
       .build();
 
     TermLogger::init(OPT.verbose.log_level().unwrap().to_level_filter(), config, TerminalMode::Mixed).unwrap();
 
-    let request_config = load_request_definition(OPT.request_config.as_str()).unwrap();
+    let mut parameter: HashMap<String, String> = HashMap::new();
+    parameter.insert("title".to_string(), "title".to_string());
+    parameter.insert("body".to_string(), "body".to_string());
+    parameter.insert("id".to_string(), "1".to_string());
+
+    let template = load_request_template(OPT.request_config.as_str()).unwrap();
+
+    validate_parameter(&template, &parameter).unwrap();
+
+    let request_config = load_request_definition(&template, &parameter).unwrap();
     debug!("{:?}", request_config);
 
     let response = make_request(request_config.url.as_str(), request_config.body, request_config.method).unwrap();
--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,6 +1,13 @@
-use log::{debug};
-use serde::{Serialize, Deserialize};
+use log::debug;
+use serde::{Deserialize, Serialize};
 use serde_yaml;
+use std::{
+    collections::{HashMap, HashSet},
+    error, fmt, fs,
+};
+use template::Template;
+
+mod template;
 
 #[derive(Debug, PartialEq, Serialize, Deserialize)]
 pub struct RequestConfig {
@@ -9,22 +16,91 @@ pub struct RequestConfig {
     pub method: Option<String>,
 }
 
-pub fn load_request_definition(file: &str) -> Result<RequestConfig, Box<dyn std::error::Error>> {
-    let f = std::fs::File::open(file)?;
-    let request_config: RequestConfig = serde_yaml::from_reader(f)?;
+pub fn load_request_template(filename: &str) -> Result<Template, Box<dyn std::error::Error>> {
+    let contents = fs::read_to_string(filename)?;
+
+    let request_config_template = Template::new(contents.as_str());
+    debug!("{:?}", request_config_template);
+
+    Ok(request_config_template)
+}
+
+#[derive(Debug, Clone)]
+pub struct InvalidParameter {
+    pub reason: String,
+}
+
+impl InvalidParameter {
+    fn new(msg: &str) -> InvalidParameter {
+        InvalidParameter {
+            reason: msg.to_string(),
+        }
+    }
+}
+
+impl fmt::Display for InvalidParameter {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.reason)
+    }
+}
+
+impl error::Error for InvalidParameter {}
+
+pub fn validate_parameter(
+    template: &Template,
+    parameter: &HashMap<String, String>,
+) -> Result<(), Box<dyn std::error::Error>> {
+    debug!("{:?}", parameter);
+
+    let provided_names: HashSet<_> = parameter.keys().cloned().collect();
+    let names: HashSet<_> = template.names.iter().cloned().collect();
+
+    let from_input: HashSet<_> = provided_names.difference(&names).collect();
+    let from_template: HashSet<_> = names.difference(&provided_names).collect();
+
+    if (from_input.len() > 0) {
+        debug!(
+            "Following parameters are defined but not used: {:?}",
+            from_input
+        );
+    }
+
+    if (from_template.len() > 0) {
+        Err(InvalidParameter::new(
+            &format!("Following parameters are missing: {:?}", from_template).to_string(),
+        )
+        .into())
+    } else {
+        Ok(())
+    }
+}
+
+pub fn load_request_definition(
+    template: &Template,
+    parameter: &HashMap<String, String>,
+) -> Result<RequestConfig, Box<dyn std::error::Error>> {
+    let request_config_string = template.render(parameter);
+
+    let request_config: RequestConfig = serde_yaml::from_str(request_config_string.as_str())?;
+
+    debug!("{:?}", request_config);
 
     Ok(request_config)
 }
 
-pub fn make_request(url: &str, body: Option<String>, method: Option<String>) -> Result<String, Box<dyn std::error::Error>> {
+pub fn make_request(
+    url: &str,
+    body: Option<String>,
+    method: Option<String>,
+) -> Result<String, Box<dyn std::error::Error>> {
     let client = reqwest::blocking::Client::new();
 
     let request_builder = match method.unwrap_or("GET".to_string()).to_uppercase().as_str() {
-        "DELETE"=> Ok(client.delete(url)),
-        "GET"=> Ok(client.get(url)),
-        "POST"=> Ok(client.post(url)),
-        "PUT"=> Ok(client.put(url)),
-        "PATCH"=> Ok(client.put(url)),
+        "DELETE" => Ok(client.delete(url)),
+        "GET" => Ok(client.get(url)),
+        "POST" => Ok(client.post(url)),
+        "PUT" => Ok(client.put(url)),
+        "PATCH" => Ok(client.put(url)),
         _ => Err("Unknown http method"),
     }?;
 
@@ -39,7 +115,6 @@ pub fn make_request(url: &str, body: Option<String>, method: Option<String>) -> 
 
     Ok(response.text()?)
 }
- 
 
 #[cfg(test)]
 mod tests {
